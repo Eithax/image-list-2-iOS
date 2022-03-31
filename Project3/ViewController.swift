9772a53ca5456f76318ed9e385022722086f361e
Santi <santiramb@gmail.com>
2022-03-31 11:03:13 +0200

    Project created

---
+++ Project3/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project3
+//
+//  Created by Santiago Rguez-Arias Mtnez-Berná on 24/3/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var flags = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Flag viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".png") {
+                flags.append(item)
+            }
+        }
+        
+        flags.sort()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = flags[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
